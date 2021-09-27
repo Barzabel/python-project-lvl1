8b4b2237d66922c89bec4cb2d4b620d7a1a6e33c
Barzabel <kochenko1995@mail.ru>
2021-09-27 15:21:56 +0300

    add lib  prompt

--- Makefile
+++ Makefile
@@ -12,3 +12,6 @@ publish:
 
 package-install:
 	python3.9 -m pip install --user dist/hexlet_code-0.1.0-py3-none-any.whl;
+
+package-upgrade:
+	python3.9 -m pip install --upgrade --user dist/hexlet_code-0.1.0-py3-none-any.whl;
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python  
 # -*- coding:utf-8 -*-
-
+import prompt
 
 def main():
 	print('Welcome to the Brain Games!')
-
+	name = prompt.string('May I have your name? ')
+	print("Hello, {}!".format(name))
 
 if __name__ == '__main__':
     main()
