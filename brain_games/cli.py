d0ce9b265ef528eaf0e154e81c2d86d036f2a8da
Barzabel <kochenko1995@mail.ru>
2021-09-28 09:14:11 +0300

    add linker

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,11 @@
-import prompt
+# coding=utf-8
 
+import prompt
+""" greeting users, ask name"""
 
 
 def welcome_user():
-	print('Welcome to the Brain Games!')
-	name = prompt.string('May I have your name? ')
-	print("Hello, {}!".format(name))
+    """ greeting users, ask name """
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print("Hello, {}!".format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,8 +2,10 @@
 # -*- coding:utf-8 -*-
 from brain_games import cli
 
+
 def main():
-	cli.welcome_user()
+    cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
