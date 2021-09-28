9269f8903cb54b4223b4732f84a86e944a62ee46
Barzabel <kochenko1995@mail.ru>
2021-09-28 14:00:48 +0300

    add new game - brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from brain_games import checking_for_even
 from brain_games import cli
 
-
 def main():
-    cli.welcome_user()
-    
+    name = cli.welcome_user()
+    checking_for_even.check_even(name)
 
 
 if __name__ == '__main__':
