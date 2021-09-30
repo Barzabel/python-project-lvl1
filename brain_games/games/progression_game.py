2d72a53b7b5c09837d1c788268e26702b7deb674
Barzabel <kochenko1995@mail.ru>
2021-09-30 07:49:33 +0300

    linter

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -3,12 +3,12 @@ import random
 
 def progression_game():
     """What number is missing in the progression?"""
-    size = random.randint(6,11)
-    step = random.randint(2,13)
+    size = random.randint(6, 11)
+    step = random.randint(2, 13)
     start = random.randint(1,100)
-    progression = [start + (x * step) for x in range(0,size)]
-    index = random.randint(0,size - 1)
+    progression = [start + (x * step) for x in range(0, size)]
+    index = random.randint(0, size - 1)
     answer = progression[index]
     progression[index] = ".."
-    qustion = " ".join(map(str,progression))
+    qustion = " ".join(map(str, progression))
     return qustion, str(answer)
