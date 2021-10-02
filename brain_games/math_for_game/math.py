456aaf12433e63b575d60ca47846a91bcbf3be70
Barzabel <kochenko1995@mail.ru>
2021-10-02 23:10:32 +0300

    fixed def ferma

--- brain_games/math_for_game/math.py
+++ brain_games/math_for_game/math.py
@@ -13,6 +13,8 @@ def get_nod(a, b):
 def ferma(x):
     if x in [1, 2, 3, 5, 7]:
         return True
+    elif x == 0:
+        return False
     for _ in range(100):
         a = random.randint(2, x - 1)
         if get_nod(x, a) != 1:
