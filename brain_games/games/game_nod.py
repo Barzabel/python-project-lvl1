c8c6bd1a7ba7430e6f71c115cbc38547bcba3974
Barzabel <kochenko1995@mail.ru>
2023-08-06 22:11:37 +0300

    fix division by zero

--- brain_games/games/game_nod.py
+++ brain_games/games/game_nod.py
@@ -5,8 +5,8 @@ from ..math_for_game.math import get_nod
 
 def game_nod() -> None:
     """Find the greatest common divisor of given numbers."""
-    rand1 = random.randint(0, 100)
-    rand2 = random.randint(0, 100)
+    rand1 = random.randint(1, 100)
+    rand2 = random.randint(1, 100)
     question = f"Question: {rand1} {rand2}"
     ansver = get_nod(rand1, rand2)
     return question, str(ansver)
