e9ff7b6a99cfc01bb6a4e7b338a8b9886d57f631
Barzabel <kochenko1995@mail.ru>
2023-08-07 16:43:12 +0300

    fix cycle of game

--- brain_games/games/base_loop.py
+++ brain_games/games/base_loop.py
@@ -5,7 +5,7 @@ NUMBER_OF_ROUNDS = 3
 
 
 def cycle_of_game(game, description):
-    is_won = True
+
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print("Hello, {}!".format(name))
@@ -15,12 +15,10 @@ def cycle_of_game(game, description):
         print(question)
         answer_user = prompt.string('Your answer:')
         if answer_user != ansver_true:
-            is_won = False
+            print(f"'{answer_user}' is wrong answer ;(."\
+            f"Correct answer was '{ansver_true}'.")
+            print(f"Let's try again, {name}!")
             break
         print('correct')
     else:
-        print(f"'{answer_user}' is wrong answer ;(.\
-         Correct answer was '{ansver_true}'.")
-        print(f"Let's try again, {name}!")
-    if is_won:
         print(f'Congratulations, {name}!')
