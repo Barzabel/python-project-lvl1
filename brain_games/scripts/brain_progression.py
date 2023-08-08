bcd07b7b817304335527ff5697f4653237ccdf4a
Barzabel <kochenko1995@mail.ru>
2023-08-08 19:37:42 +0300

    fix worng name

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from brain_games.games import game_progression
+from brain_games.games import progression_game
 from brain_games.games.base_loop import cycle_of_game
 
 
 def main():
-    cycle_of_game(game_progression)
+    cycle_of_game(progression_game)
 
 
 if __name__ == '__main__':
