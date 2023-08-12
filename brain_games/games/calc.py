fe188db05faa6decbdd67b9ac68ce08a06e9dbb0
Barzabel <kochenko1995@mail.ru>
2023-08-12 09:52:13 +0300

    fix name in calc module

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,14 +5,14 @@ DESCRIPTION = "What is the result of the expression?"
 
 
 def generate_data_for_round():
-    operat = {
+    operator = {
         "+": lambda x, y: x + y,
         "*": lambda x, y: x * y,
         "-": lambda x, y: x - y,
     }
     random_number1 = random.randint(0, 100)
     random_number2 = random.randint(0, 100)
-    operator = random.choice(["+", "*", "-"])
-    question = f"{random_number1} {operator} {random_number2}"
-    right_answer = operat[operator](random_number1, random_number2)
+    random_operator = random.choice(["+", "*", "-"])
+    question = f"{random_number1} {random_operator} {random_number2}"
+    right_answer = operat[random_operator](random_number1, random_number2)
     return question, str(right_answer)
